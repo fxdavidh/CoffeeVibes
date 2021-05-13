57fab4ffd236bdfb4de20a6c96101657303f9c48
fxdavidh <fxdavidh@gmail.com>
2021-05-13 21:56:18 +0700

    Added model

--- src/connection/Connect.java
+++ src/connection/Connect.java
@@ -1,17 +1,69 @@
 package connection;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
+import java.sql.*;
 
-public class Connect {
-	public static Connection connect() {
-		Connection connection = null;
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost/ooad", "root", "");
+public final class Connect {
+	
+	private final String USERNAME = "root"; // change with your MySQL username, the default username is 'root'
+	private final String PASSWORD = ""; // change with your MySQL password, the default password is empty
+	private final String DATABASE = "ooad"; // change with the database name that you use
+	private final String HOST = "localhost:3306"; // change with your MySQL host, the default port is 3306
+	private final String CONECTION = String.format("jdbc:mysql://%s/%s", HOST, DATABASE);
+	
+	private Connection con;
+	private Statement st;
+	
+
+    public Connect() {
+    	try {  
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection(CONECTION, USERNAME, PASSWORD);  
+            st = con.createStatement(); 
+        } catch(Exception e) {
+        	e.printStackTrace();
+        	System.out.println("Failed to connect the database, the system is terminated!");
+        	System.exit(0);
+        }  
+    }
+
+    /**
+	* This method is used for SELECT SQL statements.
+	* @param String This is the query statement
+	* @return ResultSet This returns result data from the database
+	*/
+    public ResultSet executeQuery(String query) {
+        ResultSet rs = null;
+    	try {
+            rs = st.executeQuery(query);
+        } catch(Exception e) {
+        	e.printStackTrace();
+        }
+        return rs;
+    }
+
+	/**
+	* This method is used for INSERT, UPDATE, or DELETE SQL statements.
+	* @param String This is the query statement
+	*/
+    public void executeUpdate(String query) {
+    	try {
+			st.executeUpdate(query);
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		return connection;
-	}
+    }
+    
+	/**
+	* This method is used for SELECT, INSERT, UPDATE, or DELETE SQL statements using prepare statement.
+	* @param String This is the query statement
+	*/
+    public PreparedStatement prepareStatement(String query) {
+    	PreparedStatement ps = null;
+    	try {
+			ps = con.prepareStatement(query);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+    	return ps;
+    }
 }
